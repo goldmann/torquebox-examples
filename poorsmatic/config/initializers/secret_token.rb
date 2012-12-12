91b197eae71948d100fc02299fab01dfc77c6a37
Marek Goldmann <marek.goldmann@gmail.com>
2012-12-12 09:46:21 +0100

    Initial default Rails app for poorsmatic

---
+++ poorsmatic/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Poorsmatic::Application.config.secret_token = '1270491c7a3c9e074ea595923a5a3cc0842ebed9563f69bfcf7c35d4f4a1baa2c0662a3a0b584e38add7d249835ef838ef2b21cbb201e4484e5d4cb565ebe199'
